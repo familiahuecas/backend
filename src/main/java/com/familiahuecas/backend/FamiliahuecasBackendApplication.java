5e299e65dd74e679c2cc9cfe9bd4c2c72fb719d3
Victor Manuel Huecas Sánchez de Molina <vmhuecas@cestel.es>
2024-11-12 13:52:12 +0100

    Inicialización del proyecto Spring Boot

---
+++ src/main/java/com/familiahuecas/backend/FamiliahuecasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.familiahuecas.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamiliahuecasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamiliahuecasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/familiahuecas/backend/FamiliahuecasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familiahuecas.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamiliahuecasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
